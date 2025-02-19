7a52124c1cfc069ffd0ea7db443cf41499f8ec56
bokney <toknyk@gmail.com>
2025-02-19 16:50:17 +0000

    added some docstrings

--- src/example.py
+++ src/example.py
@@ -9,14 +9,24 @@ testing sphinx documentation generation 🆗
 
 class ExampleClass:
     """
-    An example class
+    An example class. It takes the users name then
+    greets the user when the greet() method is called.
+
+    :param name: Name of individual to be greeted.
+    :type name: str
 
     Attributes:
         name (str): instance name
     """
 
     def __init__(self, name: str):
+        """
+        Constructor method.
+        """
         self.name = name
 
     def greet(self):
+        """
+        Prints message greeting name provided.
+        """
         print(f"Hello {self.name}! 🙋‍♂️")
